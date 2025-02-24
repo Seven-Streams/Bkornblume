afc8416edbe81963ec4b4dcddee60fafaee85fee
DarkSharpness <darksharpnessxzy@gmail.com>
2025-02-24 06:42:52 +0000

    minor: minor fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
-#include "error.hpp"
 #include "program.hpp"
+#include "utils/error.hpp"
 #include <iostream>
 
 int main() {
     Bkornblume::Program program;
     try {
         program.Parser();
-    } catch (Bkornblume::MyError &e) {
-        std::cout << e.GetErrorMsg() << std::endl;
+    } catch (const Bkornblume::ErrorBase &e) {
+        std::cerr << e.what() << std::endl;
         return -1;
     }
     return 0;
